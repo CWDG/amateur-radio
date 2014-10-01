83cc2738740d911a20715ad4f169d075ab34f661
Joseph Warner <hardolaf@hardolaf.com>
2014-10-01 19:41:44 -0400

    Real work

--- Gemfile
+++ Gemfile
@@ -1,13 +1,4 @@
 source "https://rubygems.org"
 
-gem 'capistrano'
-gem 'data_mapper'
-gem 'dm-core'
-gem 'dm-sqlite-adapter'
-gem 'dm-timestamps'
 gem 'haml'
-gem 'sass'
-gem 'shotgun'
 gem 'sinatra'
-gem 'sqlite3'
-
--- main.rb
+++ main.rb
@@ -1,19 +1,7 @@
-require 'data_mapper'
-require 'dm-core'
-require 'dm-migrations'
-require 'dm-sqlite-adapter'
-require 'dm-timestamps'
-require 'ostruct'
+require 'sinatra'
 
+set :haml, :format => :html5
 
-class Hash
-  def self.to_ostructs(obj, memo={})
-    return obj unless obj.is_a? Hash
-    os = memo[obj] = OpenStruct.new
-    obj.each { |k,v| os.send("#{k}=", memo[v] || to_ostructs(v, memo)) }
-    os
-  end
+get '/' do
+  haml :index
 end
-
-$config = Hash.to_ostructs(YAML.load_file(File.join(Dir.pwd, 'config.yml')))
-
