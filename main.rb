cd0f79d886f88bd84f6cbe06d68c552502491794
Joseph Warner <hardolaf@hardolaf.com>
2014-10-08 17:37:03 -0400

    Add empty 404 handling and cache control

--- main.rb
+++ main.rb
@@ -1,7 +1,22 @@
 require 'sinatra'
 
+# Set haml to compile to html5
 set :haml, :format => :html5
+
+# Explicitly set the public directory
+set :public_folder, File.dirname(__FILE__) + '/public'
+
+# Set up assets
+before do
+  cache_control :public, :must_revalidate, :max_age => 60
+end
 
 get '/' do
   haml :index
+end
+
+# Handle page not found requests nicely.
+not_found do
+	status 404
+	haml :oops
 end
