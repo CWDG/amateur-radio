bd79efa3e5142e630e4f3a53584c2327cd7562b7
Joseph Warner <hardolaf@hardolaf.com>
2014-10-17 11:40:30 -0400

    Fix sample Foundation file's CSS

--- server.js
+++ server.js
@@ -2,21 +2,34 @@ var express        = require('express');
 var app            = express();
 var winston        = require('winston');
 var expressWinston = require('express-winston');
-var methodOverride = require('method-override');
 
-// Set up body parsing and method overrides
-app.use(methodOverride());
+// Set up error handling function
+function clientErrorHandler(err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send({ error: 'Something blew up!' });
+  } else {
+    next(err);
+  }
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err });
+}
 
 // Set up winston for logging
 app.use(expressWinston.logger({
   transports: [
     new winston.transports.Console({
-      json: true,
+      level: 'debug',
+      handleExceptions: true,
+      json: false,
       colorize: true
     })
   ],
   meta: false,
-  msg: "HTTP {{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}",
+  msg: "HTTP {{res.statusCode}} FROM {{req.connection.remoteAddress}} {{req.method}} {{res.responseTime}}ms {{req.url}}",
+  exitOnError: false
 //  expressFormat: true,
 //  colorStatus: true,
 }));
@@ -31,10 +44,21 @@ app.use(expressWinston.errorLogger({
 	],
 }));
 
+// Set up error handling
+app.use(clientErrorHandler);
+app.use(errorHandler);
+
 // Set the rendering engine (jade)
 app.engine('jade', require('jade').__express);
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
+
+// I like my generated HTML pretty so I can see where I made mistakes in
+// templating
+app.use(function(req, res, next) {
+  app.locals.pretty = true;
+  next();
+});
 
 // Set up static asset directories
 app.use(express.static(__dirname + '/public'));
@@ -54,9 +78,9 @@ app.get('/humans.txt', function(req, res) {
 
 // Begin main routing functions
 app.get('/', function (req, res) {
-	res.render('index', { title: 'Hey', message: 'Hello there!'});
-	res.end();
-})
+  res.render('index', { title: 'Home' });
+  res.end();
+});
 
 // Start the server
 var server = app.listen(3000, function() {
