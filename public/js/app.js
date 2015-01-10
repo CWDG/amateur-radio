5bdd9b6ba0484012899dc35a38ddebd4f63ead2d
Joseph Warner <hardolaf@hardolaf.com>
2015-01-09 20:32:52 -0500

    Use graphical logo

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,9 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+if (!Modernizr.svg) {
+  $('img[src*="svg"]').attr('src', function() {
+    return $(this).attr('src').replace('.svg', '.png');
+  });
+}
